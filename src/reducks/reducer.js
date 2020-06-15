3b903137f769d5642c2b2b2f6bb78256404e766c
david <davidwnewman91@gmail.com>
2020-06-15 11:28:47 -0500

    pre-server save

--- src/reducks/reducer.js
+++ src/reducks/reducer.js
@@ -19,13 +19,14 @@ const initialState ={
         {campaign: 'TOA', player: 'Baum', name: 'Elaana', class: 'Fighter', role: 'Damage', lvl: 1, max_hp: 12, ac: 16, current_hp: 12, temp_hp: 0, str: 8, dex: 17, con: 14, int: 11, wis: 13, cha: 12, health: 'ok'}, 
         {campaign: 'TOA', player: 'ItsVlad', name: 'Thwip', class: 'Rogue', role: 'Damage', lvl: 1, max_hp: 10, ac: 14, current_hp: 10, temp_hp: 0, str: 10, dex: 17, con: 14, int: 13, wis: 13, cha: 8, health: 'ok'}
         ,
-        {campaign: 'Winds of Change', player: 'AutoDave', name: 'unknown', class: 'unknown', role: 'unknown', lvl: 0, max_hp: 0, ac: 0, current_hp: 0, temp_hp: 0, str: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0, health: 'ok'},
-        {campaign: 'Winds of Change', player: 'CodyNo', name: 'unknown', class: 'unknown', role: 'unknown', lvl: 0, max_hp: 0, ac: 0, current_hp: 0, temp_hp: 0, str: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0, health: 'ok'},
-        {campaign: 'Winds of Change', player: 'SwordsaintIIV', name: 'unknown', class: 'unknown', role: 'unknown', lvl: 0, max_hp: 0, ac: 0, current_hp: 0, temp_hp: 0, str: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0, health: 'ok'},
-        {campaign: 'Winds of Change', player: 'Wonsnot', name: 'unknown', class: 'unknown', role: 'unknown', lvl: 0, max_hp: 0, ac: 0, current_hp: 0, temp_hp: 0, str: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0, health: 'ok'},
-        {campaign: 'Winds of Change', player: 'Baum', name: 'unknown', class: 'unknown', role: 'unknown', lvl: 0, max_hp: 0, ac: 0, current_hp: 0, temp_hp: 0, str: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0, health: 'ok'}, 
-        {campaign: 'Winds of Change', player: 'ItsVlad', name: 'unknown', class: 'unknown', role: 'unknown', lvl: 0, max_hp: 0, ac: 0, current_hp: 0, temp_hp: 0, str: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0, health: 'ok'},
-        {campaign: 'Winds of Change', player: 'ml1201', name: 'unknown', class: 'unknown', role: 'unknown', lvl: 0, max_hp: 0, ac: 0, current_hp: 0, temp_hp: 0, str: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0, health: 'ok'}
+        {campaign: 'Winds of Change', player: 'AutoDave', name: 'Gnaagark', class: 'Barbarian', role: 'Tank', lvl: 3, max_hp: 25, ac: 15, current_hp: 25, temp_hp: 0, str: 17, dex: 14, con: 16, int: 10, wis: 8, cha: 8, health: 'ok'},
+        {campaign: 'Winds of Change', player: 'CodyNo', name: 'Throfir', class: 'Fighter', role: 'Tank', lvl: 2, max_hp: 20, ac: 18, current_hp: 20, temp_hp: 0, str: 17, dex: 12, con: 14, int: 10, wis: 12, cha: 12, health: 'ok'},
+        {campaign: 'Winds of Change', player: 'SwordsaintIIV', name: 'Serr', class: 'Fighter', role: 'Tank', lvl: 3, max_hp: 23, ac: 19, current_hp: 23, temp_hp: 0, str: 15, dex: 16, con: 13, int: 11, wis: 14, cha: 9, health: 'ok'},
+        {campaign: 'Winds of Change', player: 'Wonsnot', name: 'Thorren', class: 'Wizard', role: 'Damage', lvl: 3, max_hp: 14, ac: 12, current_hp: 14, temp_hp: 0, str: 8, dex: 14, con: 12, int: 16, wis: 12, cha: 14, health: 'ok'},
+        {campaign: 'Winds of Change', player: 'Baum', name: 'Mia', class: 'Fighter', role: 'Damage', lvl: 3, max_hp: 23, ac: 16, current_hp: 23, temp_hp: 0, str: 16, dex: 10, con: 14, int: 10, wis: 8, cha: 15, health: 'ok'}, 
+        {campaign: 'Winds of Change', player: 'ItsVlad', name: 'Crane', class: 'Bard', role: 'Damage', lvl: 3, max_hp: 18, ac: 14, current_hp: 18, temp_hp: 0, str: 8, dex: 14, con: 10, int: 12, wis: 14, cha: 17, health: 'ok'},
+        {campaign: 'Winds of Change', player: 'ml1201', name: 'Zealvari', class: 'Cleric', role: 'Healer', lvl: 2, max_hp: 18, ac: 18, current_hp: 18, temp_hp: 0, str: 14, dex: 8, con: 14, int: 13, wis: 17, cha: 17, health: 'ok'}
+        // {campaign: 'Winds of Change', player: 'Will', name: 'Dog', class: 'Companion', role: 'Damage', lvl: 0, max_hp: 0, ac: 0, current_hp: 0, temp_hp: 0, str: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0, health: 'ok'}
     ]
 }
 
