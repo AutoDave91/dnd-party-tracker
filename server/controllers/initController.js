65cff45b2b065c350f1ff77e17f2a794bbf7a47d
david <davidwnewman91@gmail.com>
2020-07-06 11:17:14 -0500

    init check

--- server/controllers/initController.js
+++ server/controllers/initController.js
@@ -9,16 +9,6 @@ function rolledInit(req, res){
     let value = +hp_change
 
     if(initiative[0]){
-        console.log('hit')
-        // for(let i =0; i<initiative.length-1; i++){
-        //     if(initiative[i].name !== name){
-        //         console.log(initiative)
-        //         initiative.push({name, value})
-        //         initiative.sort((a, b)=>(a.value > b.value) ? -1 : 1)
-        //     } 
-        //     // else {
-        //     // }
-        // }
         let rolled = initiative.find(character => character.name == name)
         if(rolled == undefined){
             initiative.push({name, value})
