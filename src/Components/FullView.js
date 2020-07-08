85c2d80945514e1b9109931dbb52c2b0ffe5bcba
david <davidwnewman91@gmail.com>
2020-07-08 15:51:39 -0500

    ready for Axios and sql commands

--- src/Components/FullView.js
+++ src/Components/FullView.js
@@ -3,83 +3,114 @@ import React from 'react';
 import '../style/full_view.css'
 
 function Full_View(props){
-    // console.log(props.member.img)
+    console.log(props.member)
+    // let {campaign_id, character_id, token, character_name, party_role, lvl, character_class, max_hp, character_ac, current_hp, temp_hp, strength, dex, con, intel, wis, cha, health, active} = props.state
     return(
-        <main className='member' id={props.member.party_role}>
-            <section className='pic-name'>
-                {`../assets/${props.member.character_name}.JPG` ?
-                    <img className='party-pics' src={require('../assets/' + props.member.character_name + '.JPG')} alt={props.member.character_name} />
-                    :
-                    <image className='party-pics' src={require('../assets/Pending.JPG')} alt={props.member.character_name} />
-                }
-                    <div className='name-class-player'>
-                        <div className='slant-top'>
-                            <h1 className='name'>{props.member.character_name}</h1>
-                        </div>
-                        <div className='slant-bottom'>
-                            <h2 className='class'>{props.member.class} ({props.member.lvl})</h2>
-                            {props.component === 'Campaign' ? (
-                                // <h2 className='player'>controlled by: {props.member.player}</h2>
-                            <h2 className='player'>Party: {props.member.party_role}</h2>
-                            ) : (
-                                <button className='player' onClick={props.editToggle}>Edit</button>
-                            )}
-                        </div>
-                    </div>
-            </section>
-            <section className='character-info'>
+        <main className='FullView'>
+            {props.edit === true && props.state.character_id === props.member.character_id ? (
+                <div className='member' id='editCharacter'>
+                <h3>Token Image</h3>
+                <h3>Name: <input onChange={props.handleChange} className='character_name' placeholder={props.state.character_name} /></h3>
+                <h3>Class: <input onChange={props.handleChange} className='character_class' placeholder={props.state.character_class} /></h3>
+                <h3>Party Role: <input onChange={props.handleChange} className='party_role' placeholder={props.state.party_role} /></h3>
+                <h3>Level: <input onChange={props.handleChange} className='lvl' placeholder={props.state.lvl} /></h3>
+                <h3>Max HP: <input onChange={props.handleChange} className='max_hp' placeholder={props.state.max_hp} /></h3>
+                <h3>AC: <input onChange={props.handleChange} className='character_ac' placeholder={props.state.character_ac} /></h3>
+                <h3>Current HP: <input onChange={props.handleChange} className='current_hp' placeholder={props.state.current_hp} /></h3>
+                <h3>Temp HP: <input onChange={props.handleChange} className='temp_hp' placeholder={props.state.temp_hp} /></h3>
+                <h3>STR: <input onChange={props.handleChange} className='strength' placeholder={props.state.strength} /></h3>
+                <h3>DEX: <input onChange={props.handleChange} className='dex' placeholder={props.state.dex} /></h3>
+                <h3>CON: <input onChange={props.handleChange} className='con' placeholder={props.state.con} /></h3>
+                <h3>INT: <input onChange={props.handleChange} className='intel' placeholder={props.state.intel} /></h3>
+                <h3>WIS: <input onChange={props.handleChange} className='wis' placeholder={props.state.wis} /></h3>
+                <h3>CHA: <input onChange={props.handleChange} className='cha' placeholder={props.state.cha} /></h3>
+                <h3>Status for next session: <select onChange={props.handleChange} className='active'>
+                    <option value={true}>Here</option>
+                    <option value={false}>Away</option>
+                </select></h3>
+                <button onClick={props.handleClick} className='submit'>Submit</button>
+            </div>
+            ) : (
+                <div className='member' id={props.member.party_role}>
+                    <section className='pic-name'>
+                        {`../assets/${props.member.character_name}.JPG` ?
+                            <img className='party-pics' src={require('../assets/' + props.member.character_name + '.JPG')} alt={props.member.character_name} />
+                            :
+                            <image className='party-pics' src={require('../assets/Pending.JPG')} alt={props.member.character_name} />
+                        }
+                            <div className='name-class-player'>
+                                <div className='slant-top'>
+                                    <h1 className='name'>{props.member.character_name}</h1>
+                                </div>
+                                <div className='slant-bottom'>
+                                    <h2 className='class'>{props.member.class} ({props.member.subclass}) ({props.member.lvl})</h2>
+                                    {/* <h2 className='class'>{props.member.subclass}</h2> */}
+                                    {props.component === 'Campaign' ? (
+                                        // <h2 className='player'>controlled by: {props.member.player}</h2>
+                                    <h2 className='player'>Party Role: {props.member.party_role}</h2>
+                                    ) : (
+                                        <button className='edit' onClick={props.editToggle}>Edit</button>
+                                    )}
+                                </div>
+                            </div>
+                    </section>
+                    <section className='character-info'>
+                        
+                        <section className='ac-hp-1'>
+                            <div className='ac-group'>
+                                <h2 className='ac-title'>AC</h2>
+                                <h2 className='ac'>{props.member.ac}</h2>
+                            </div>
+                            <div className='max-hp'>
+                                <h2 className='max'>Max HP</h2>
+                                <h2 className='hp'>{props.member.max_hp}</h2>
+                            </div>
+                        </section>
+                        <section className='ac-hp-2'>
+                            <div className='temp-hp'>
+                                <h2 className='temp'>Temp HP</h2>
+                                <h2 className='hp'>{props.member.temp_hp}</h2>
+                            </div>
+                            <div className='current-hp'>
+                                <h2 className='current'>Current HP</h2>
+                                <h2 className='hp'>{props.member.current_hp}</h2>
+                            </div>
+                        </section>
+                        {props.crit === true ? 
+                            <div></div>
+                        : 
+                            <ul className='ability-scores'>
+                                <section className='ability-score'>
+                                    <li>STR</li>
+                                    <li className='ability-points'>{props.member.strength}</li>
+                                </section>
+                                <section className='ability-score'>
+                                    <li>DEX</li>
+                                    <li className='ability-points'>{props.member.dex}</li>
+                                </section>
+                                <section className='ability-score'>
+                                    <li>CON</li>
+                                    <li className='ability-points'>{props.member.con}</li>
+                                </section>
+                                <section className='ability-score'>
+                                    <li>INT</li>
+                                    <li className='ability-points'>{props.member.intel}</li>
+                                </section>
+                                <section className='ability-score'>
+                                    <li>WIS</li>
+                                    <li className='ability-points'>{props.member.wis}</li>
+                                </section>
+                                <section className='ability-score'>
+                                    <li>CHA</li>
+                                    <li className='ability-points'>{props.member.cha}</li>
+                                </section>
+                            </ul>
+                        }
+                    </section>
+                </div>
+
                 
-                <section className='ac-hp-1'>
-                    <div className='ac-group'>
-                        <h2 className='ac-title'>AC</h2>
-                        <h2 className='ac'>{props.member.ac}</h2>
-                    </div>
-                    <div className='max-hp'>
-                        <h2 className='max'>Max HP</h2>
-                        <h2 className='hp'>{props.member.max_hp}</h2>
-                    </div>
-                </section>
-                <section className='ac-hp-2'>
-                    <div className='temp-hp'>
-                        <h2 className='temp'>Temp HP</h2>
-                        <h2 className='hp'>{props.member.temp_hp}</h2>
-                    </div>
-                    <div className='current-hp'>
-                        <h2 className='current'>Current HP</h2>
-                        <h2 className='hp'>{props.member.current_hp}</h2>
-                    </div>
-                </section>
-                {props.crit === true ? (
-                    <div></div>
-                ) : (
-                    <ul className='ability-scores'>
-                    <section className='ability-score'>
-                        <li>STR</li>
-                        <li className='ability-points'>{props.member.strength}</li>
-                    </section>
-                    <section className='ability-score'>
-                        <li>DEX</li>
-                        <li className='ability-points'>{props.member.dex}</li>
-                    </section>
-                    <section className='ability-score'>
-                        <li>CON</li>
-                        <li className='ability-points'>{props.member.con}</li>
-                    </section>
-                    <section className='ability-score'>
-                        <li>INT</li>
-                        <li className='ability-points'>{props.member.intel}</li>
-                    </section>
-                    <section className='ability-score'>
-                        <li>WIS</li>
-                        <li className='ability-points'>{props.member.wis}</li>
-                    </section>
-                    <section className='ability-score'>
-                        <li>CHA</li>
-                        <li className='ability-points'>{props.member.cha}</li>
-                    </section>
-                </ul>
-                    )}
-            </section>
+            )}
         </main>
     )
 }
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -12,6 +12,7 @@ class Profile extends Component {
         this.state = {
             characters: [],
             edit: false,
+            new: false,
             campaign_id: 0,
             character_id: 0,
             token: '',
@@ -33,7 +34,8 @@ class Profile extends Component {
             active: true
         }
         this.handleClick = this.handleClick.bind(this);
-        this.engageToggle = this.engageToggle.bind(this);
+        this.editToggle = this.editToggle.bind(this);
+        this.addToggle = this.addToggle.bind(this)
         this.handleChange = this.handleChange.bind(this);
     }
 
@@ -47,88 +49,72 @@ class Profile extends Component {
     handleClick(){
         // Send Axios request with state to either add or edit a character. Use character_id to know which is required (edit or create)
         // use reducer's state from session to send in the player_id to link characters to their user.
-        this.setState({edit: false})
+        this.setState({edit: false, new: false})
     }
     handleChange(e){
         // handle inputs
         this.setState({[e.target.name]: e.target.value})
         console.log(this.state)
     }
-    engageToggle(member){
+    editToggle(member){
         // console.log(member)
         let character_class = member.class
         let {character_id, token, character_name, party_role, lvl, max_hp, ac, current_hp, temp_hp, strength, dex, con, intel, wis, cha} = member
         this.setState({edit: true, character_id, token, character_name, character_class, party_role, lvl, max_hp, character_ac: ac, current_hp, temp_hp, strength, dex, con, intel, wis, cha})
         // console.log(this.state)
     }
+    addToggle(){
+        // console.log(member)
+        this.setState({new: true})
+        // console.log(this.state)
+    }
 
     render(){
-        let {campaign_id, character_id, token, character_name, party_role, lvl, character_class, max_hp, character_ac, current_hp, temp_hp, strength, dex, con, intel, wis, cha, health, active} = this.state
+        // let {campaign_id, character_id, token, character_name, party_role, lvl, character_class, max_hp, character_ac, current_hp, temp_hp, strength, dex, con, intel, wis, cha, health, active} = this.state
         return(
             <main className='Profile'>
                 <h1>{this.props.reducer.user.username}'s Characters</h1>
+                {this.state.new === true ?
+                    (
+                        <section className='member' id='createCharacter'>
+                            <select onChange={this.handleChange} className='campaign_id'>
+                                <option value='1'>TOA</option>
+                                <option value='2'>Winds of Change</option>
+                            </select>
+                            <h3>token image uploads coming soon</h3>
+                            <input onChange={this.handleChange} className='character_name' placeholder='Name' />
+                            <input onChange={this.handleChange} className='character_class' placeholder='Class' />
+                            <input onChange={this.handleChange} className='party_role' placeholder='Party Role' />
+                            <input onChange={this.handleChange} className='lvl' placeholder='Level' />
+                            <input onChange={this.handleChange} className='max_hp' placeholder='Max HP' />
+                            <input onChange={this.handleChange} className='character_ac' placeholder='AC' />
+                            <input onChange={this.handleChange} className='current_hp' placeholder='Current HP' />
+                            <input onChange={this.handleChange} className='temp_hp' placeholder='Temp HP' />
+                            <input onChange={this.handleChange} className='strength' placeholder='STR' />
+                            <input onChange={this.handleChange} className='dex' placeholder='DEX' />
+                            <input onChange={this.handleChange} className='con' placeholder='CON' />
+                            <input onChange={this.handleChange} className='intel' placeholder='INT' />
+                            <input onChange={this.handleChange} className='wis' placeholder='WIS' />
+                            <input onChange={this.handleChange} className='cha' placeholder='CHA' />
+                            <select onChange={this.handleChange} className='active'>
+                                <option value='true'>Here</option>
+                                <option value='false'>Away</option>
+                            </select>
+                            <button onClick={this.handleClick} className='submit'>Create Character</button>
+                        </section>
+                    ) : <button className='new' onClick={()=>this.addToggle()}>Add New Character</button>
+                }
                 <section className='userCharacters'>
                     {this.state.characters[0] ? (
                         this.state.characters.map((member, i) =>(
-                            this.state.edit === false ? (
+                            // this.state.edit === false ? (
                                 <div className='userCharacters'>
-                                    <FullView member={member} i={i} component={'Profile'} editToggle={()=>this.engageToggle(member)}/>
+                                    <FullView member={member} i={i} component={'Profile'} editToggle={()=>this.editToggle(member)} edit={this.state.edit} state={this.state} handleChange={this.handleChange} handleClick={this.handleClick}/>
                                 </div>
-                            ) : (
-                                <div className='member' id='editCharacter'>
-                                    <h3>Token Image</h3>
-                                    <h3>Name: <input onChange={this.handleChange} className='character_name' placeholder={character_name} /></h3>
-                                    <h3>Class: <input onChange={this.handleChange} className='character_class' placeholder={character_class} /></h3>
-                                    <h3>Party Role: <input onChange={this.handleChange} className='party_role' placeholder={party_role} /></h3>
-                                    <h3>Level: <input onChange={this.handleChange} className='lvl' placeholder={lvl} /></h3>
-                                    <h3>Max HP: <input onChange={this.handleChange} className='max_hp' placeholder={max_hp} /></h3>
-                                    <h3>AC: <input onChange={this.handleChange} className='character_ac' placeholder={character_ac} /></h3>
-                                    <h3>Current HP: <input onChange={this.handleChange} className='current_hp' placeholder={current_hp} /></h3>
-                                    <h3>Temp HP: <input onChange={this.handleChange} className='temp_hp' placeholder={temp_hp} /></h3>
-                                    <h3>STR: <input onChange={this.handleChange} className='strength' placeholder={strength} /></h3>
-                                    <h3>DEX: <input onChange={this.handleChange} className='dex' placeholder={dex} /></h3>
-                                    <h3>CON: <input onChange={this.handleChange} className='con' placeholder={con} /></h3>
-                                    <h3>INT: <input onChange={this.handleChange} className='intel' placeholder={intel} /></h3>
-                                    <h3>WIS: <input onChange={this.handleChange} className='wis' placeholder={wis} /></h3>
-                                    <h3>CHA: <input onChange={this.handleChange} className='cha' placeholder={cha} /></h3>
-                                    <h3>Status for next session: <select onChange={this.handleChange} className='active'>
-                                        <option>Here</option>
-                                        <option>Away</option>
-                                    </select></h3>
-                                    <button onClick={this.handleClick} className='submit'>Submit</button>
-                                </div>
-                            )
-                                
                         ))
                     ) : (
                         <h1>Contact AutoDave to add your character</h1>
                     )}
-                </section>
-                <section className='member' id='createCharacter'>
-                    <select onChange={this.handleChange} className='campaign_id'>
-                        <option value='1'>TOA</option>
-                        <option value='2'>Winds of Change</option>
-                    </select>
-                    <h3>token image uploads coming soon</h3>
-                    <input onChange={this.handleChange} className='character_name' placeholder='Name' />
-                    <input onChange={this.handleChange} className='character_class' placeholder='Class' />
-                    <input onChange={this.handleChange} className='party_role' placeholder='Party Role' />
-                    <input onChange={this.handleChange} className='lvl' placeholder='Level' />
-                    <input onChange={this.handleChange} className='max_hp' placeholder='Max HP' />
-                    <input onChange={this.handleChange} className='character_ac' placeholder='AC' />
-                    <input onChange={this.handleChange} className='current_hp' placeholder='Current HP' />
-                    <input onChange={this.handleChange} className='temp_hp' placeholder='Temp HP' />
-                    <input onChange={this.handleChange} className='strength' placeholder='STR' />
-                    <input onChange={this.handleChange} className='dex' placeholder='DEX' />
-                    <input onChange={this.handleChange} className='con' placeholder='CON' />
-                    <input onChange={this.handleChange} className='intel' placeholder='INT' />
-                    <input onChange={this.handleChange} className='wis' placeholder='WIS' />
-                    <input onChange={this.handleChange} className='cha' placeholder='CHA' />
-                    <select onChange={this.handleChange} className='active'>
-                        <option value='true'>Here</option>
-                        <option value='false'>Away</option>
-                    </select>
-                    <button onClick={this.handleClick} className='submit'>Create Character</button>
                 </section>
             </main>
         )
